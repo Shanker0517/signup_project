bd41e1cb0b37a2b2f3353aef45169b2ca8e44912
shankerk <shankerk.in@mouritech.com>
2024-02-08 18:19:33 +0530

    form validation with error handling from frontend & backend and that error response binded to template

---
+++ src/app/interfaces/userinterface.ts
@@ -0,0 +1,10 @@
+import { NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
+
+export interface Userinterface {
+    firstName: string;
+    lastName: string;
+    email: string;
+    dateofbirth:Date;
+    phoneno:number;
+    password:string;
+}
